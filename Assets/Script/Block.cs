c70af8fd16c5b3a7381cbc6a423eb9ebc16246ea
kaytopod <kaytopod0119@gmail.com>
2024-05-30 19:56:10 +0900

    2024年5月30日追加

--- Assets/Script/Block.cs
+++ Assets/Script/Block.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class Block : MonoBehaviour
 {
+    public int score = 10;
     //何かとぶつかったときビルトインメソッド
     private void OnCollisionEnter(Collision collision)
     {
+        ScoreScript.instance.ScrerManager(score);   
+
         //ゲームオブジェクトを削除
         Destroy(gameObject);
     }
